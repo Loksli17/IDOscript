60e1292a3d4da2087f6449bf3a9b7b22cdb8b75d
RobinLoskli <ami0504@mail.ru>
2021-02-15 21:14:38 +1000

    scan full

--- index.js
+++ index.js
@@ -21,7 +21,33 @@ let
 // 1: какой вид выхода лучше сделать?
 // 2: желательно заиметь шаблон страницы
 
+
+const
+    //* return true or false;
+    checkFolder = (folderName) => {
+        try {
+            const fileData = fs.statSync(folderName);
+            if(fileData.isFile()){
+                console.error(`Файл ${folderName} НЕ БЫЛ ОБРАБОТАН потому что не папка.`);
+                return false;
+            }else{
+                return true;
+            }
+        }catch(error){
+            throw new Error(error);
+        }
+    };
+
+// console.log(checkFolder('././Центры/УМО/1. Экспл.тяг.подвижного состава/~$ Организация работы Нарядчиков-2017.doc'));
+
 let 
+    // !ШАГ3
+    //TODO: создает html страницы
+    createFiles = () => {
+
+    },
+
+    // !ШАГ2
     //TODO: получает на вход html страницу, возвращает ее копию заполненную данными
     fillLayout = (layout /*string*/) => {
         for(let key in data){
@@ -31,34 +57,36 @@ let
         return layout;
     },
 
-    //TODO: бежит по всем папкам (корневая папка Центры) и собирает в них информацию по 
-    searchData = (folderName /*string*/) => {
-
-        try {
-            let fileData = fs.statSync(folderName);
-            console.log(fileData);   
-        }catch(error){
-            
-        }
-
+    // !ШАГ1
+    //TODO: бежит по всем папкам (корневая папка Центры) и собирает в них информацию
+    scanFolder = (folderName /*string*/) => {
+        if(!checkFolder(folderName)) return;
         let centers = fs.readdirSync(folderName);
         
         centers.forEach((center) => {
-            console.log(center);
+            if(!checkFolder(`./${folderName}/${center}`)) return;
             let directions = fs.readdirSync(`./${folderName}/${center}`);
 
+            console.log(center);
+
             directions.forEach((direction) => {
-                console.log('__', direction);
+                if(!checkFolder(`./${folderName}/${center}/${direction}`)) return;
                 let workingPrograms = fs.readdirSync(`./${folderName}/${center}/${direction}`);
 
+                console.log('__', direction);
+
                 workingPrograms.forEach((program) => {
+                    if(!checkFolder(`./${folderName}/${center}/${direction}/${program}`)) return true;
                     console.log('____', program);
+                    let dataFiles = fs.readdirSync(`./${folderName}/${center}/${direction}/${program}`);
+
+                    //сбор данных по програме
                 })
             });
         })
     };
 
 
-searchData('./Центры');
+scanFolder('./Центры');
 
 
