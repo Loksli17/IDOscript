de123dfde8494f1db1cebe0ef134fd513ad8a8c4
RobinLoskli <ami0504@mail.ru>
2021-02-15 22:51:33 +1000

    create data for program progress

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 const fs = require('fs');
 
 let
-    layoutArr    = [],
-    globalLayout = fs.readFileSync('layout.html', 'utf-8'),
-    data         = {
-        name           : 'name',
-        annotation     : 'annotation',
-        formOfEducation: 'formOfEducation',
-        amountOfHours  : 'amountOfHours',
-        cost           : 'cost',
-        documentType   : 'documentType',
-        documentExample: 'documentExample',
-        plan           : 'plan',
-        workingProgram : 'workingProgram',
-        image          : 'image',
-        titleList      : 'titleList',
+    layoutArr      = [],
+    globalLayout   = fs.readFileSync('layout.html', 'utf-8'),
+    structureFiles = {
+        1: 'Название.txt',
+        2: 'Анотация.txt',
+        3: 'Форма обучения.txt',
+        4: 'Кол-во часов.txt',
+        5: 'Стоимость.txt',
+        6: 'Тип выдаваемого документа.txt',
+        7: 'Образец документа.jpg',
+        8: 'Учебный палн.doc',
+        9: 'Рабочая программа.doc',
+        10: 'Картинка',
+        11: 'Титульный лист.txt',
+    },
+    programData = {
+        'Название'               : 'name',
+        'Анотация'               : 'annotation',
+        'Форма обучения'         : 'formOfEducation',
+        'Кол-во часов'           : 'amountOfHours',
+        'Стоимость'              : 'cost',
+        'Тип выдаемого документа': 'documentType',
+        'Образец документа'      : 'documentExample',
+        'Учебный план'           : 'plan',
+        'Рабочая программа'      : 'workingProgram',
+        'Картинка'               : 'image',
+        'Титульный лсит'         : 'titleList',
     };
 
 //вопросы: 
 // 1: какой вид выхода лучше сделать?
 // 2: желательно заиметь шаблон страницы
+// 3: индексы зачем есть 9_1
 
 
 const
@@ -39,25 +53,56 @@ const
     },
 
     //* return data about progect 
-    getProgramData = () => {
+    getProgramData = (filesName /*Array<string>*/, path /*string*/) => {
+        
+        let program = {};
 
+        for(let i = 0; i < filesName.length; i++){
+            let 
+                file         = filesName[i],
+                id           = file.match(/^[0-9]+\s/g),
+                propWithType = file.match(/([а-я]|\s|-)+.(txt|doc|pdf|jpg)/ig),
+                prop         = file.match(/([а-я]|\s|-)+/ig);
+
+            if(id == null){
+                return;
+            }
+
+            id           = Number(id[0]);
+            prop         = prop[0];
+            propWithType = propWithType[0]; 
+
+            console.log('++++++++', file, id, prop, propWithType);
+            //ошибка!!!!!
+            console.log(structureFiles[id].length, propWithType.length);
+            if(structureFiles[id] != propWithType){
+                console.error(`Файл ${file} не подходит под стандартную структуру`);
+                return;
+            }
+
+            //read file
+        //    console.log(fs.readFileSync(`${path}/${propWithType}`));
+            
+        }
+
+        return program;
     }
 
-// console.log(checkFolder('././Центры/УМО/1. Экспл.тяг.подвижного состава/~$ Организация работы Нарядчиков-2017.doc'));
+
 
 let 
     // !ШАГ3
     //TODO: создает html страницы
-    createFiles = (filesName /*Array<string>*/) => {
+    createFiles = () => {
 
     },
 
     // !ШАГ2
     //TODO: получает на вход html страницу, возвращает ее копию заполненную данными
     fillLayout = (layout /*string*/) => {
-        for(let key in data){
+        for(let key in programData){
             let regExp = new RegExp(`\{\{${key}\}\}`, 'g');
-            layout = str.replace(regExp, data[key]);
+            layout = str.replace(regExp, programData[key]);
         }
         return layout;
     },
@@ -85,8 +130,8 @@ let
                     console.log('____', program);
                     let dataFiles = fs.readdirSync(`./${folderName}/${center}/${direction}/${program}`);
                     
-                    console.log(getProgramData(dataFiles));
-                    //сбор данных по програме
+                    console.log(getProgramData(dataFiles, `./${folderName}/${center}/${direction}/${program}`));
+                    //сбор данных по программе
                 })
             });
         })
